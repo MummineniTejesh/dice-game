22f474899f8b6864652b8eb97eeb5a56d44957f8
MummineniTejesh <tejeshmummineni@gmail.com>
2022-05-19 12:53:19 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-var ranOne=Math.floor(Math.random()*6);
-var ranTwo=Math.floor(Math.random()*6);
-var one=ranOne+1;
-var two=ranTwo+1;
-document.querySelector(".one").src="dice"+one+".png";
-document.querySelector(".two").src="dice"+two+".png";
-if(one>two)
-{
-    document.querySelector(".winner").innerHTML="Player one wins";
-    document.querySelector(".flag").src="flag.png";
-}
-else if(two>one)
-{
-    document.querySelector(".winner").innerHTML="Player two wins";
-    document.querySelector(".flag").src="flag.png";
-}
-else
-{
-    document.querySelector(".winner").innerHTML="It's a draw";
+document.querySelector("button").addEventListener("click",roll);
+
+function roll(){
+    var ranOne=Math.floor(Math.random()*6);
+    var ranTwo=Math.floor(Math.random()*6);
+    var one=ranOne+1;
+    var two=ranTwo+1;
+    document.querySelector(".one").src="dice"+one+".png";
+    document.querySelector(".two").src="dice"+two+".png";
+    if(one>two)
+    {
+        document.querySelector(".winner").innerHTML="🏆 Player one wins";
+    }
+    else if(two>one)
+    {
+        document.querySelector(".winner").innerHTML="Player two wins 🏆 ";
+    }
+    else
+    {
+        document.querySelector(".winner").innerHTML="It's a draw";
+    }
 }
